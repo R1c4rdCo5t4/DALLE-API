b49dec755c4c88d7888fb271e0827165cea083dd
Ricardo <r1c4rd03646@gmail.com>
2022-11-05 02:50:47 +0000

    UI Change, Token Prompt, Download Image

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 // import { Configuration, OpenAIApi } from "/node_modules/openai"
 
+
+// browserify main.js -o out.js
+
 const { Configuration, OpenAIApi } = require("openai")
 
-
 let generateImageRequest = async () => {
-    const input = document.getElementById("prompt").value;
+
+
+    document.getElementById("prompt-img").src = ""
+    const prompt = document.getElementById("prompt").value;
+    const token = document.getElementById("token").value;
+
 
     const configuration = new Configuration({
-        apiKey: "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",
+        apiKey: token,
     })
     const openai = new OpenAIApi(configuration);
 
     const response = await openai.createImage({
-        prompt: input,
+        prompt: prompt,
         n: 1,
         size: "512x512",
     })
@@ -20,7 +27,26 @@ let generateImageRequest = async () => {
     let image_url = response.data.data[0].url;
 
     document.getElementById("prompt-img").src = image_url
-    document.getElementById("prompt-img").alt = input
+    document.getElementById("prompt-img").alt = prompt
+    
+    document.getElementById("download-link").href = image_url;
+ 
+    
+    
+    
 }
 
 document.getElementById("prompt-submit").addEventListener("click", generateImageRequest)
+
+// function changeLink(id, link) {
+
+//     var link = document.getElementById(id);
+
+//     window.open(
+//       link.href,
+//       '_blank'
+//     );
+
+//     link.setAttribute('href', link);
+// }
+// changeLink("download-link", image_url)
--- out.js
+++ out.js
@@ -187,19 +187,26 @@ process.umask = function() { return 0; };
 },{}],2:[function(require,module,exports){
 // import { Configuration, OpenAIApi } from "/node_modules/openai"
 
+
+// browserify main.js -o out.js
+
 const { Configuration, OpenAIApi } = require("openai")
 
-
 let generateImageRequest = async () => {
-    const input = document.getElementById("prompt").value;
+
+
+    document.getElementById("prompt-img").src = ""
+    const prompt = document.getElementById("prompt").value;
+    const token = document.getElementById("token").value;
+
 
     const configuration = new Configuration({
-        apiKey: "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",
+        apiKey: token,
     })
     const openai = new OpenAIApi(configuration);
 
     const response = await openai.createImage({
-        prompt: input,
+        prompt: prompt,
         n: 1,
         size: "512x512",
     })
@@ -207,10 +214,29 @@ let generateImageRequest = async () => {
     let image_url = response.data.data[0].url;
 
     document.getElementById("prompt-img").src = image_url
-    document.getElementById("prompt-img").alt = input
+    document.getElementById("prompt-img").alt = prompt
+    
+    document.getElementById("download-link").href = image_url;
+ 
+    
+    
+    
 }
 
 document.getElementById("prompt-submit").addEventListener("click", generateImageRequest)
+
+// function changeLink(id, link) {
+
+//     var link = document.getElementById(id);
+
+//     window.open(
+//       link.href,
+//       '_blank'
+//     );
+
+//     link.setAttribute('href', link);
+// }
+// changeLink("download-link", image_url)
 },{"openai":38}],3:[function(require,module,exports){
 module.exports = require('./lib/axios');
 },{"./lib/axios":5}],4:[function(require,module,exports){
